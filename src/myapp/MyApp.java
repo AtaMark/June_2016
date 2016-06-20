2f8bcd09242eaa1b07b0c91fbfb1a88c930f856c
Atabulwaire John <jatabulwaire@adroitltd.com>
2016-06-20 18:51:09 +0300

    MyApp

--- src/myapp/MyApp.java
+++ src/myapp/MyApp.java
@@ -16,7 +16,7 @@ public class MyApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println();
+        System.out.println("I am me");
     }
     
 }
